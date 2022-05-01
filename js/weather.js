66ddb8578d1cbb780d21c6c96205ea501655079b
Jon-a-than <2140145186@qq.com>
2022-05-01 15:40:45 +0800

    Site updated: 2022-05-01 15:40:43

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "B6D7A8",
+    "tmpSize": "16",
+    "cityColor": "EB8846",
+    "citySize": "16",
+    "aqiColor": "CCCC7A",
+    "aqiSize": "16",
+    "weatherIconSize": "40",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1", "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "left",
+    "key": "bc8c9e0c560c412ba604ac852a7e6d4b"
+  }
+}
+
+document.querySelector('.d-none').innerHTML = '<div id="he-plugin-simple"></div><script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>'
