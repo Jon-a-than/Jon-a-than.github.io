b0630d3c6db1d34cb1e06132f91eaf7859270c8e
Jon-a-than <2140145186@qq.com>
2022-05-02 13:38:52 +0800

    Site updated: 2022-05-02 13:38:52

--- js/watch.js
+++ js/watch.js
@@ -1,4 +1,11 @@
 document.title = '今夕何夕💌见此良人'
 window.onblur = () => document.title = '春草明年绿💔王孙归不归'
 window.onfocus = () => document.title = '今夕何夕💌见此良人'
-console.log(222)
+
+const print = (colorCode, input) => 
+  console.log(`\x1b[0;${colorCode}m${input}\x1b[0m`)
+
+setTimeout(() => {
+  console.clear()
+  print(91, '今夕何夕💌见此良人')
+}, 1000)
