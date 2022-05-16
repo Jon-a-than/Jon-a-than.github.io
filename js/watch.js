a36da7fb5daa4c4f3c9cba6c614b48ad02c6f679
Jon-a-than <2140145186@qq.com>
2022-05-16 18:01:09 +0800

    Site updated: 2022-05-16 18:01:07

--- js/watch.js
+++ js/watch.js
@@ -5,7 +5,7 @@ window.onfocus = () => document.title = '今夕何夕💌见此良人'
 const print = (colorCode, input) => 
   console.log(`\x1b[0;${colorCode}m${input}\x1b[0m`)
 
-setTimeout(() => {
-  console.clear()
-  print(91, '今夕何夕💌见此良人')
-}, 1000)
+// setTimeout(() => {
+//   console.clear()
+//   print(91, '今夕何夕💌见此良人')
+// }, 1000)
